b24e7e5d43b6d775d75fc8bb69a1b52b19dfbe3a
YgshBerwal <yogeshberwal266@gmail.com>
2025-05-30 17:32:42 +0530

    Added route handling

--- vehicle-parking-app/app.py
+++ vehicle-parking-app/app.py
@@ -5,7 +5,9 @@ from flask_security import Security, SQLAlchemyUserDatastore, auth_required
 
 
 def createApp():
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder='frontend', static_folder='frontend', static_url_path='/static')
+    
+
 
     app.config.from_object(LocalDevelopmentConfig)
 
@@ -15,7 +17,7 @@ def createApp():
     #flask security
     datastore = SQLAlchemyUserDatastore(db, User, Role)
 
-    app.security = Security(app, datastore=datastore)
+    app.security = Security(app, datastore=datastore, register_blueprint = False)
     app.app_context().push()
 
     return app
@@ -23,16 +25,7 @@ def createApp():
 app = createApp()
 
 import backend.models.create_initial_data
-
-@app.get('/')
-def home():
-    return '<h1> home page</h1>'
-
-@app.get('/protected')
-@auth_required()
-def protected():
-    return '<h1> only accessible by auth user</h1>'
-
+import backend.routes
 
 if (__name__ == '__main__'):
     app.run()
