95b033c3c1bf3c671ce09d42b89a212a055eeedf
YgshBerwal <yogeshberwal266@gmail.com>
2025-06-02 15:01:54 +0530

    Logout functionality added

--- vehicle-parking-app/frontend/components/Navbar.js
+++ vehicle-parking-app/frontend/components/Navbar.js
@@ -4,6 +4,7 @@ export default {
             <router-link to='/'>Home</router-link>
             <router-link to='/login'>Login</router-link>
             <router-link to='/register'>Register</router-link>
+            <button class="btn btn-secondary" @click="$store.commit('logout')" > Logout </button>
         </div>
     `
 }
