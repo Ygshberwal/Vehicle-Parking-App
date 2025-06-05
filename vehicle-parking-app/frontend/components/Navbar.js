c0c43af6a9d20b86f40e95e5f846c367db8ca620
YgshBerwal <yogeshberwal266@gmail.com>
2025-06-05 15:28:24 +0530

    Styled Navbar

--- vehicle-parking-app/frontend/components/Navbar.js
+++ vehicle-parking-app/frontend/components/Navbar.js
@@ -1,18 +1,51 @@
 export default {
-    template : `
-        <div>
-            <router-link to='/'>Home</router-link>
+  template: `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid d-flex justify-content-between align-items-center">
 
-            <router-link v-if="!$store.state.loggedIn" to='/login'>Login</router-link>
-            <router-link v-if="!$store.state.loggedIn" to='/register'>Register</router-link>
+        <!-- Logo on Left -->
+        <router-link class="navbar-brand" to="/">ParkingApp</router-link>
 
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'admin'" to='/admin-dashboard'>AdminDashboard</router-link>
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'user'" to='/user-dashboard'>UserDashboard</router-link>
-            <router-link v-if="$store.state.loggedIn" to='/lots'>Lots</router-link>
-            <router-link v-if="$store.state.loggedIn && $store.state.role == 'user'" to='/explore'>Explore</router-link>
+        <!-- Centered Links -->
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0 d-flex flex-row gap-3">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
 
+          <template v-if="!$store.state.loggedIn">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/login">Login</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/register">Register</router-link>
+            </li>
+          </template>
 
-            <button class="btn btn-secondary" v-if="$store.state.loggedIn" @click="$store.commit('logout')">Logout</button>       
+          <template v-if="$store.state.loggedIn">
+            <li class="nav-item" v-if="$store.state.role === 'admin'">
+              <router-link class="nav-link" to="/admin-dashboard">Admin Dashboard</router-link>
+            </li>
+
+            <li class="nav-item" v-if="$store.state.role === 'user'">
+              <router-link class="nav-link" to="/user-dashboard">User Dashboard</router-link>
+            </li>
+
+            <li class="nav-item">
+              <router-link class="nav-link" to="/lots">Lots</router-link>
+            </li>
+
+            <li class="nav-item" v-if="$store.state.role === 'user'">
+              <router-link class="nav-link" to="/explore">Explore</router-link>
+            </li>
+          </template>
+        </ul>
+
+        <!-- Logout button on Right -->
+        <div v-if="$store.state.loggedIn">
+          <button class="btn btn-outline-light" @click="$store.commit('logout')">Logout</button>
         </div>
-    `
+
+      </div>
+    </nav>
+  `
 }
