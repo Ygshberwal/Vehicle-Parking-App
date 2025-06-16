73c037a65c325e9c1924dc5f5c5a7160498dc30a
YgshBerwal <yogeshberwal266@gmail.com>
2025-06-16 14:38:12 +0530

    Modified Lot Card on Admin Dashboard

--- vehicle-parking-app/frontend/components/LotCard.js
+++ vehicle-parking-app/frontend/components/LotCard.js
@@ -1,16 +1,21 @@
 export default {
-    name : "LotCard",
-    props : ['lot_id', 'location_name', 'address', 'pincode', 'price', 'available_slot'],
-    template : `
-    <div class = "jumbotron" style = "width: 300">
-        <h2 @click="$router.push('/lots/' + lot_id)"" > {{ location_name}} </h2>
-        <p> {{ address }} </p>
-        <p> ID: {{ lot_id }} </p>
-        <p> Pincode : {{pincode}} </p>
+    name: "LotCard",
+    props: { lot_id: Number, location_name: String, address: String, pincode: Number, price: Number, available_slot: {type: Number, default: 0}, occupied_slot: {type: Number,default: 0}},
+    template: `
+    <div class="jumbotron" style="width: 300px">
+        <h2 @click="$router.push('/lots/' + lot_id)" style="cursor: pointer;">
+            {{ location_name }}
+        </h2>
+        <p>{{ address }}</p>
+        <p>ID: {{ lot_id }}</p>
+        <p>Pincode: {{ pincode }}</p>
         <hr>
-        <p> Slots Available: {{available_slot}} </p>
-        <p> Price per hour : {{price}} </p>
+        <p>
+            Slots available:
+            <span style="color: green">{{ available_slot }}</span> /
+            <span>{{ available_slot + occupied_slot }}</span>
+        </p>
+        <p>Price per hour: ₹{{ price }}</p>
     </div>
-    `,
-
+    `
 }
